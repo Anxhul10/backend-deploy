7e81485e8c6d0283965dcf0132f18889c2ac3af1
anxhul10 <anxhul10@gmail.com>
2024-08-30 20:07:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/home',(req, res)=>{
+    res.send("hwlloe");
+});
+app.get('/start',(req, res)=>{
+    res.send('<h1>header tag</h1>')
+})
+app.listen(process.env.PORT, ()=>{
+    console.log(`port is starting at ${port}`);
+})
